05a90836d5aad062df3c3ff26d11a68f18a95a3e
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2012-11-20 17:29:34 +1300

    Fixes for Travis build

--- ghcjs-build.sh
+++ ghcjs-build.sh
@@ -6,10 +6,10 @@ make distclean || true
 # Normal GHC Build
 perl boot
 ./configure --prefix=$HOME/ghcjs
-make -j5 || make -j5 || make -j5 || make -j5
+make -j5 || make
 
 # Build Stage 3 which has been set up to include GHCJS
-make -j5 stage=3 || make -j5 stage=3 || make -j5 stage=3 || make -j5 stage=3
+make -j5 stage=3 || make stage=3
 
 # Replace the stage 1 and 2 binaries with the stage 3 one
 cp inplace/lib/ghc-stage3 inplace/lib/ghc-stage1
@@ -19,7 +19,7 @@ cp ghc/stage3/build/tmp/ghc-stage3 ghc/stage2/Build/tmp/ghc-stage2
 rm -rf compiler/stage2
 rm -rf libraries/*/dist-install
 rm -rf libraries/*/*/dist-install
-make -j5 || make -j5 || make -j5 || make -j5
+make -j5 || make
 
 # Replace the stage 1 and 2 binaries with the stage 3 one
 cp inplace/lib/ghc-stage3 inplace/lib/ghc-stage2
@@ -28,7 +28,7 @@ cp ghc/stage3/build/tmp/ghc-stage3 ghc/stage2/Build/tmp/ghc-stage2
 # Delete the stuff that was built without JavaScript
 rm -rf libraries/haskell98/dist-install
 rm -rf libraries/haskell2010/dist-install
-make -j5 || make -j5 || make -j5 || make -j5
+make -j5 || make
 
 # Install
 make install
