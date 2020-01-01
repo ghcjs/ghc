4391c10b1e21647b7b89864259e5ef968105845c
Luite Stegeman <stegeman@gmail.com>
2020-01-01 17:46:15 +0900

    add -host-package-db flag

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -3872,6 +3872,8 @@ package_flags_deps = [
         ------- Packages ----------------------------------------------------
     make_ord_flag defFlag "package-db"
       (HasArg (addPkgConfRef . PkgConfFile))
+  , make_ord_flag defFlag "host-package-db"
+      (HasArg (addPkgConfRef . HostPkgConfFile))
   , make_ord_flag defFlag "clear-package-db"      (NoArg clearPkgConf)
   , make_ord_flag defFlag "no-global-package-db"  (NoArg removeGlobalPkgConf)
   , make_ord_flag defFlag "no-user-package-db"    (NoArg removeUserPkgConf)
@@ -5260,6 +5262,9 @@ data PkgConfRef
   = GlobalPkgConf
   | UserPkgConf
   | PkgConfFile FilePath
+  | HostPkgConfFile FilePath -- ^a package config file that specifies
+                             --  packages compiled for the same host as
+                             --  the compiler.
   deriving Eq
 
 addPkgConfRef :: PkgConfRef -> DynP ()
@@ -5423,6 +5428,10 @@ parseEnvFile envfile = mapM_ parseEntry . lines
   where
     parseEntry str = case words str of
       ("package-db": _)     -> addPkgConfRef (PkgConfFile (envdir </> db))
+        -- relative package dbs are interpreted relative to the env file
+        where envdir = takeDirectory envfile
+              db     = drop 11 str
+      ("host-package-db": _) -> addPkgConfRef (HostPkgConfFile (envdir </> db))
         -- relative package dbs are interpreted relative to the env file
         where envdir = takeDirectory envfile
               db     = drop 11 str
